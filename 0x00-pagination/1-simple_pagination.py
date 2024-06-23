a9a1f213659373456d0fc927530a7ddc07f9581e
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-24 00:41:19 +0300

    Import index_page and remove definition

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,13 +1,10 @@
+#!/usr/bin/env python3
 import csv
 import math
 from typing import List, Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Returns a tuple containing the start and end index"""
-    start = (page - 1) * page_size
-    end = start + page_size
-    return (start, end)
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
