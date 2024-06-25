6d85cc15d7157b30af2e454288ed6e81df2913b3
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-26 01:11:58 +0300

    Add solution for task 4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""LRUCache class"""
+"""MRU Caching"""
 from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
-    """LRUCache class"""
+class MRUCache(BaseCaching):
+    """MRU Caching"""
 
     def __init__(self):
         """ Initiliaze
@@ -15,15 +15,15 @@ class LRUCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key is not None or item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+        if key is not None and item is not None:
+            if (
+                len(self.cache_data) >= BaseCaching.MAX_ITEMS
+                    and key not in self.cache_data):
                 last = self.order.pop(0)
                 del self.cache_data[last]
                 print("DISCARD: {}".format(last))
             self.cache_data[key] = item
-            self.order.append(key)
-        else:
-            return
+            self.order.insert(0, key)
 
     def get(self, key):
         """ Get an item by key
@@ -31,5 +31,5 @@ class LRUCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return None
         self.order.remove(key)
-        self.order.append(key)
+        self.order.insert(0, key)
         return self.cache_data.get(key)
