ffb7498793f56821314648f8aaf838fd1ab0b86d
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-26 00:41:59 +0300

    Add solution for task 3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""LRUCache class"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """LRUCache class"""
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None or item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last = self.order.pop(0)
+                del self.cache_data[last]
+                print("DISCARD: {}".format(last))
+            self.cache_data[key] = item
+            self.order.append(key)
+        else:
+            return
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.order.remove(key)
+        self.order.append(key)
+        return self.cache_data.get(key)
