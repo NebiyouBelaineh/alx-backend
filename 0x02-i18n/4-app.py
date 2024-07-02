8e614a7e53aefa66c2980f2f931e80de50ed339b
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-02 18:25:55 +0300

    Add solution for task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -21,12 +21,16 @@ app.config.from_object(Config)
 @app.route('/', strict_slashes=False)
 def index():
     """Basic flask app"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 @babel.localeselector
 def get_locale():
     """Get locale from request header"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
